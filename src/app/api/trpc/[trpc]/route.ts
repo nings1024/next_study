34c1b9f45157be4a8cfa71196e3c19ab9f0f596e
马宁宁 <maningning@sinosoft.com.cn>
2025-04-15 17:43:26 +0800

    修改字体

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -3,11 +3,13 @@ import { appRouter } from '@/server';
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 
 const handler = (req: Request) =>
-  fetchRequestHandler({
+{
+  console.log('请求url',req.url)
+  return fetchRequestHandler({
     endpoint: '/api/trpc',
     req,
     router: appRouter,
     createContext: () => ({}),
-  });
+  })};
 
 export { handler as GET, handler as POST };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
 import Provider from "./_trpc/Provider";
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,10 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-      <Provider>{children}</Provider>  
+      {/* 使用 Tailwind 定义的 font-sans / font-mono，已从 CSS 变量绑定 */}
+      <body className="antialiased font-sans">
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
