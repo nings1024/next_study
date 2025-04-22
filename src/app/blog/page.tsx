8ffc87e5829ebc7b3ad31abf5ad9a0450eebec96
马宁宁 <maningning@sinosoft.com.cn>
2025-04-22 18:08:05 +0800

    设置自签名证书

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,4 @@
-import { pool } from '@/app/lib/db';
+
 import { RowDataPacket } from 'mysql2';
 
 interface JiraTask extends RowDataPacket {
@@ -7,16 +7,7 @@ interface JiraTask extends RowDataPacket {
 }
 
 export default async function Blog() {
-  const [rows] = await pool.query<JiraTask[]>(
-    "SELECT JiraCode, `Desc` FROM jiratask j WHERE j.JiraCode = 'LIS-143980'"
-  );
 
-  if (rows.length === 0) return <div>Not found</div>;
+ return <div>Not found</div>;
 
-  return (
-    <>
-      <h1>{rows[0].JiraCode}</h1>
-      <p>{rows[0].Desc}</p>
-    </>
-  );
 }
